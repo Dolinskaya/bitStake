150385dc2dbe41142f0b35aba5f6d9d0550b0936
Juliya <enotuxa@yandex.ru>
2017-08-07 19:24:04 +0300

    new design

--- bitstake_cabinet/js/script.js
+++ bitstake_cabinet/js/script.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
     $('.kard-wrapper').find('.item--hover').click(function(){
         console.log('test');
         $(this).css({'background': '#fff',
-            '-webkit-box-shadow': '0px -6px 9px -5px rgba(241,241,241,1), 0px 9px 9px -5px rgba(241,241,241,1), -5px 0px 9px -2px rgba(241,241,241,1)'});
+            '-webkit-box-shadow': '0px -6px 9px -5px rgba(241,241,241,1), 0px 9px 9px -5px rgba(241,241,241,1), -5px 0px 9px -2px rgba(241,241,241,1)',
+            '-moz-box-shadow': '0px -6px 9px -5px rgba(241,241,241,1), 0px 9px 9px -5px rgba(241,241,241,1), -5px 0px 9px -2px rgba(241,241,241,1)',
+            'box-shadow': '0px -6px 9px -5px rgba(241,241,241,1), 0px 9px 9px -5px rgba(241,241,241,1, -5px 0px 9px -2px rgba(241,241,241,1)',
+            'border-left': '5px solid #660080'
+        });
     });
 });
