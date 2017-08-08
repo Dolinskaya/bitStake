1e5e9c52b93849b2e55ab0e960af43cc2f958503
Juliya <enotuxa@yandex.ru>
2017-08-08 11:17:49 +0300

    bug fix

--- bitstake_cabinet/js/script.js
+++ bitstake_cabinet/js/script.js
@@ -1,14 +1,9 @@
 $(document).ready(function(){
-    $('.items').on('click', '.item--hover', function(){
+    $('.items').on('click', '.item--hover', function(e){
         console.log('test');
-        $(this).css({'background': '#fff',
-            '-webkit-box-shadow': '0px -6px 9px -5px rgba(241,241,241,1), 0px 9px 9px -5px rgba(241,241,241,1), -5px 0px 9px -2px rgba(241,241,241,1)',
-            '-moz-box-shadow': '0px -6px 9px -5px rgba(241,241,241,1), 0px 9px 9px -5px rgba(241,241,241,1), -5px 0px 9px -2px rgba(241,241,241,1)',
-            'box-shadow': '0px -6px 9px -5px rgba(241,241,241,1), 0px 9px 9px -5px rgba(241,241,241,1, -5px 0px 9px -2px rgba(241,241,241,1)',
-            'border-left': '5px solid #660080'
-        });
-        $(this).find('p').css('font-family', 'Proxima Nova Bold');
-        $(this).find('span').css('font-family', 'Proxima Nova Bold');
+        $(this).toggleClass('active');
+        $(this).find('p').toggleClass('active-font');
+        $(this).find('span').toggleClass('active-font');
     })
 });
 // , function(){
@@ -18,3 +13,9 @@ $(document).ready(function(){
 //         'border-left': 'none'
 //     })
 
+// $(this).css({'background': '#fff',
+//     '-webkit-box-shadow': '0px -6px 9px -5px rgba(241,241,241,1), 0px 9px 9px -5px rgba(241,241,241,1), -5px 0px 9px -2px rgba(241,241,241,1)',
+//     '-moz-box-shadow': '0px -6px 9px -5px rgba(241,241,241,1), 0px 9px 9px -5px rgba(241,241,241,1), -5px 0px 9px -2px rgba(241,241,241,1)',
+//     'box-shadow': '0px -6px 9px -5px rgba(241,241,241,1), 0px 9px 9px -5px rgba(241,241,241,1, -5px 0px 9px -2px rgba(241,241,241,1)',
+//     'border-left': '5px solid #660080'
+// });
