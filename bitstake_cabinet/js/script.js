76e0ecf3357914d33c622d57d9611fbaf98b0793
Juliya <enotuxa@yandex.ru>
2017-08-08 10:25:56 +0300

    bug fix

--- bitstake_cabinet/js/script.js
+++ bitstake_cabinet/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('.kard-wrapper').find('.item--hover').click(function(){
+    $('.items').on('click', '.item--hover', function(){
         console.log('test');
         $(this).css({'background': '#fff',
             '-webkit-box-shadow': '0px -6px 9px -5px rgba(241,241,241,1), 0px 9px 9px -5px rgba(241,241,241,1), -5px 0px 9px -2px rgba(241,241,241,1)',
@@ -7,5 +7,12 @@ $(document).ready(function(){
             'box-shadow': '0px -6px 9px -5px rgba(241,241,241,1), 0px 9px 9px -5px rgba(241,241,241,1, -5px 0px 9px -2px rgba(241,241,241,1)',
             'border-left': '5px solid #660080'
         });
-    });
+    })
 });
+// , function(){
+//     $(this).css({'background': 'none',
+//         '-webkit-box-shadow': 'none',
+//         '-moz-box-shadow': 'none',
+//         'border-left': 'none'
+//     })
+
