92ca83d7a02d7afae602c20eaa95a10cbcccbd6b
Juliya <enotuxa@yandex.ru>
2017-08-17 13:47:07 +0300

    валидация формы

--- js/script.js
+++ js/script.js
@@ -4,11 +4,18 @@ if (navigator.userAgent.indexOf('Android' || 'Iphone' || 'Blackberry' || 'Webos'
 if( screen.width <= 320 ) {
     location.assign('http://bitstake.ru/m/mobile.html');
 };
+function isValid() {
+    var errorBlock = '<a href="" class="validate">Поле не заполнено</a>';
+    $('.field-for-validate').each(function () {
+        if($(this).find('input').val() === ''){
+            $(this).append(errorBlock);
+        }
+    });
+};
 $(document).ready(function(){
   $('.command__slider').slick({
     autoplay: false,
     infinite: true,
-    // slidesToShow: 1,
     centerMode: true,
     variableWidth: true,
     nextArrow: '<img src="img/arrow-next.png" alt="" class="btn-ar slick-next">',
@@ -28,4 +35,27 @@ $(document).ready(function(){
             scrollTop: $(to).offset().top
         }, 800);
     });
+    $('.nav__question').hover(function () {
+        $(this).find('img').attr('src', 'img/col-hover.png');
+    }, function () {
+        $(this).find('img').attr('src', 'img/col.png');
+    });
+    $('.device-btn-more').hover(function () {
+        console.log('hover');
+       $(this).find('img').attr('src', 'img/pink-plus--hover.png');
+    }, function () {
+        $(this).find('img').attr('src', 'img/pink-plus.png');
+    });
+    $('.title-block__item').hover(function () {
+        $(this).prev().attr('src', 'img/mail--hover.png');
+    }, function () {
+        $(this).prev().attr('src', 'img/mail.png');
+    });
+    $('.connect__form-btn').on('click', function (e) {
+        e.preventDefault();
+        isValid();
+    });
+    $('.validate').click(function (e) {
+        e.preventDefault();
+    })
 });
